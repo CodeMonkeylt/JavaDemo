7164e492c2751021f0f7ccf79d0782839487d2f8
lt <taoliunancy@gmail.com>
2017-04-13 13:31:02 +0800

    repair the coding

--- src/com/monkey/study/enumDiscovery/enumDemo.java
+++ src/com/monkey/study/enumDiscovery/enumDemo.java
@@ -4,16 +4,16 @@ package com.monkey.study.enumDiscovery;
 * @ClassName: enumDemo 
 * @Description: demo for enum
 * @author lt
-* @date 2017��4��7�� ����4:04:23
+* @date 2017年4月13日 下午1:28:02
  */
 public enum enumDemo {
-	MON("��һ"),
-	TUE("�ܶ�"),
-	WED("����"),
-	THU("����"),
-	FRI("����"),
-	SAT("����"),
-	SUN("����");
+	MON("周一"),
+	TUE("周二"),
+	WED("周三"),
+	THU("周四"),
+	FRI("周五"),
+	SAT("周六"),
+	SUN("周日");
 	
 	private String value;
 
